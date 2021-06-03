5401998cf31843fbd4998b9722d0f8d1e426d018
rijkvp <rijkvanputten@hotmail.nl>
2021-06-03 21:00:42 +0200

    Add game struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 use raylib::prelude::*;
 
+const WINDOW_WIDTH: i32 = 1020;
+const WINDOW_HEIGHT: i32 = 800;
 
-fn main() {
-    let (mut rl, thread) = raylib::init()
-        .size(640, 480)
-        .title("Hello, World")
-        .build();
+struct Game {
+    rl: RaylibHandle,
+    thread: RaylibThread,
+}
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+impl Game {
+    fn new() -> Self {
+        let (mut rl, thread) = raylib::init()
+            .size(WINDOW_WIDTH, WINDOW_HEIGHT)
+            .title("Repair Game")
+            .build();
+
+        Self {
+            rl,
+            thread,
+        }
+    }
+    
+    fn init(&mut self) {
+         
+    }
+
+    fn update(&mut self) {
+
+    }
+
+    fn draw(&mut self) {
+        let mut d = self.rl.begin_drawing(&self.thread);
 
         d.clear_background(Color::WHITE);
-        d.draw_text("Repair game!", 12, 12, 20, Color::BLACK);
+        d.draw_text("Repair game!", 12, 12, 46, Color::BLACK);                
     }
+
+    fn run(mut self) {
+        self.init();
+        while !self.rl.window_should_close() {
+            self.update();
+            self.draw();
+        }
+    }
+}
+
+
+fn main() {
+    Game::new().run();
 }

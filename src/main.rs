fd103b72f19e2322797ee7f27549962d34554e7a
rijkvp <rijkvanputten@hotmail.nl>
2021-06-10 21:06:44 +0200

    Add gamedata struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
 use raylib::prelude::*;
 
 const WINDOW_WIDTH: i32 = 1020;
-const WINDOW_HEIGHT: i32 = 800;
+const WINDOW_HEIGHT : i32 = 800;
+
+#[derive(Debug)]
+struct Input {
+    input_v: f32,
+    input_h: f32,
+}
+
+struct Player {
+    pos: Vector2,
+} 
+
+impl Player {
+    fn new() -> Self {
+        Self {
+            pos: Vector2::zero()
+        }
+    }
+
+    fn update(&mut self, delta_time: f32, input: Input) {
+        self.pos += Vector2::new(input.input_h, input.input_v) * delta_time * 100.0;
+    }
+}
+
+struct GameData {
+    player: Player,
+}
+
+impl GameData {
+    fn new() -> Self {
+        Self {
+            player: Player::new()
+        }
+    }
+}
 
 struct Game {
     rl: RaylibHandle,
     thread: RaylibThread,
+    data: GameData,
 }
 
 impl Game {
@@ -18,6 +53,7 @@ impl Game {
         Self {
             rl,
             thread,
+            data: GameData::new(),
         }
     }
     
@@ -25,8 +61,34 @@ impl Game {
          
     }
 
-    fn update(&mut self) {
+    fn get_input(&mut self) -> Input 
+    {
+        let input_h = {
 
+            if self.rl.is_key_pressed(KeyboardKey::KEY_RIGHT) && self.rl.is_key_pressed(KeyboardKey::KEY_LEFT) {
+                0.0
+            } else if self.rl.is_key_pressed(KeyboardKey::KEY_RIGHT) {
+                1.0
+            } else if self.rl.is_key_pressed(KeyboardKey::KEY_LEFT) {
+                -1.0
+            } else {
+                0.0
+            }
+        };
+
+        let input_v = 0.0;
+
+        Input {
+            input_h,
+            input_v
+        }
+    }
+
+    fn update(&mut self) {
+        let input = self.get_input();
+        println!("{:?}", input); 
+        let dt = self.rl.get_frame_time();
+        self.data.player.update(dt, input);
     }
 
     fn draw(&mut self) {
@@ -34,6 +96,11 @@ impl Game {
 
         d.clear_background(Color::WHITE);
         d.draw_text("Repair game!", 12, 12, 46, Color::BLACK);                
+        Self::draw_game(&mut d, &self.data);
+    }
+
+    fn draw_game(d: &mut impl RaylibDraw, data: &GameData) {
+        d.draw_rectangle(data.player.pos.x as i32, data.player.pos.y as i32, 100, 100, Color::RED);
     }
 
     fn run(mut self) {
